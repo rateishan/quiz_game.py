234c081692283365cc898fdd95856096a233419d
rateishan <ishanrate@gmail.com>
2024-02-19 00:25:05 +0530

    added answer and qu

--- quiz_game.py
+++ quiz_game.py
@@ -3,5 +3,16 @@ playing =  input("do you want to play ? ")
 
 if playing != "yes":
     quit()
-print("okay play")    
+print("okay let's play")  
+answer = input("what does CPU stand for ? ") 
+if answer == "central processing unit" :
+    print("correct") 
+else:
+    print("incorrect")    
+answer = input("what is your home city ? ") 
+if answer == "kandy" :
+    print("correct") 
+else:
+    print("incorrect")      
+
     
